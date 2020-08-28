86d97d2638bb30cc612d261c76147c71b3de8fad
CarlosAmaral <carloslopesamaral@outlook.com>
2020-08-28 15:10:57 +0400

    Script done

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
+from datetime import timedelta
 
 CITY_DATA = {'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
@@ -68,6 +69,8 @@ def input_validator(input_str, type):
                 result = validate_filter_choice(input_value)
             if type == "city":
                 result = validate_city(input_value)
+            if type == "answer":
+                result = validate_yes_no_answer(input_value)
             if not result:
                 raise Exception("Please type one of the values above")
         except Exception as error:
@@ -92,6 +95,17 @@ def validate_day(day):
     day_options = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     return True if day.lower() in day_options else False
 
+def validate_yes_no_answer(answer):
+    answer_options = ['yes', 'no']
+    return True if answer.lower() in answer_options else False
+
+
+def get_dict_key(value, dict_list):
+    for key, val in dict_list.items():
+        if val == value:
+            return key
+
+
 def load_data(city, month=None, day=None):
     """
     Loads data for the specified city and filters by month and day if applicable.
@@ -105,17 +119,19 @@ def load_data(city, month=None, day=None):
     """
 
     df = pd.read_csv(CITY_DATA[city])
-
+    df['month'] = pd.to_datetime(df['Start Time']).dt.month
+    df['week day'] = pd.to_datetime(df['Start Time']).dt.dayofweek
     """
-    Return Data Frame if no filters applied
+    Return Data Frame with month filter
     """
-    if month == None and day == None:
-        return df
-    
     if month != None:
-        df['month'] = pd.to_datetime(df['Start Time']).dt.month
-    df = df[df['month'] == 4]
-    return df
+        month_filter = df[df['month'] == MONTHS[month.lower()]]
+        return month_filter
+    elif day != None:
+        day_filter = df[df['week day'] == DAYS[day.lower()]]
+        return day_filter
+    else:
+        return df
 
 
 def time_stats(df):
@@ -125,13 +141,19 @@ def time_stats(df):
     start_time = time.time()
 
     # display the most common month
-
+    most_common_month = df['month'].value_counts().idxmax()
 
     # display the most common day of week
-
+    most_common_week_day = df['week day'].value_counts().idxmax()
 
     # display the most common start hour
+    df['hour'] = pd.to_datetime(df['Start Time']).dt.hour
+    most_common_hour = df['hour'].value_counts().idxmax()
 
+
+    print("Most common month: {}".format(get_dict_key(most_common_month, MONTHS).title()))
+    print("Most common week day: {}".format(get_dict_key(most_common_week_day, DAYS).title()))
+    print("Most common hour: {} (24h format)".format(most_common_hour))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -144,13 +166,18 @@ def station_stats(df):
     start_time = time.time()
 
     # display most commonly used start station
-
+    most_common_start_station = df['Start Station'].value_counts().idxmax()
 
     # display most commonly used end station
-
+    most_common_end_station = df['End Station'].value_counts().idxmax()
 
     # display most frequent combination of start station and end station trip
+    df['Start and End Station Combo'] = df['Start Station'].map(str) + ' - ' + df['End Station'].map(str)
+    most_common_start_end_stations_combo = df['Start and End Station Combo'].value_counts().idxmax()
 
+    print("Most common Start station: {}".format(most_common_start_station))
+    print("Most common End station: {}".format(most_common_end_station))
+    print("Most common Start - End Station Combination: {}".format(most_common_start_end_stations_combo))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -163,10 +190,14 @@ def trip_duration_stats(df):
     start_time = time.time()
 
     # display total travel time
-
-
+    total_travel_time_seconds = df['Trip Duration'].sum()
+    total_travel_time_hours_mins = timedelta(seconds=int(total_travel_time_seconds))
     # display mean travel time
-
+    mean_travel_time_seconds = df['Trip Duration'].mean()
+    mean_travel_time_hours_mins = timedelta(seconds=int(mean_travel_time_seconds))
+    
+    print("Total travel time: {} (h/m)".format(total_travel_time_hours_mins))
+    print("Average travel time: {} (h/m)".format(mean_travel_time_hours_mins))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -179,19 +210,38 @@ def user_stats(df):
     start_time = time.time()
 
     # Display counts of user types
-
-
+    count_user_type = df['User Type'].value_counts()
+    
     # Display counts of gender
-
+    count_gender = df['Gender'].value_counts()
 
     # Display earliest, most recent, and most common year of birth
+    earliest_birth = df['Birth Year'].min()
+    most_recent_birth = df['Birth Year'].max()
+    most_common_birth = df['Birth Year'].value_counts().idxmax()
 
+    print("User Type counts:\n{}".format(count_user_type))
+    print("\nUser Gender counts:\n{}".format(count_gender))
+    print("\nEarliest birth year: {}, most recent birth year: {}, and most common birth year: {}".format(earliest_birth, most_recent_birth, most_common_birth))
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-def str_to_bold(str):
-    return "\033[1m{}\033[0m".format(str)
+
+def print_raw_data(df):
+    raw_data_answer = input_validator("\nWould you like to view raw data? Type yes or no. \n", "answer")
+    if raw_data_answer.lower() != "yes":
+        return df
+
+    start_count = 0
+    while True:
+        end_count = start_count + 5
+        print(df.iloc[start_count:end_count])
+        more_raw_data_answer = input_validator("\nWould you like to view additional raw data? Type yes or no. \n", "answer")
+        if more_raw_data_answer.lower() == "yes":
+            start_count += 5
+            continue
+        break
 
 def main():
     while True:
@@ -203,7 +253,9 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        print_raw_data(df)    
+
+        restart = input_validator("\nWould you like to restart? Type yes or no.\n", "answer")
         if restart.lower() != 'yes':
             break
 
